114b2bc63556f72820af8cf45fc75112b38a19e2
fchojna <filipchojna99@gmail.com>
2024-10-06 16:32:18 +0200

    initialize project structure

---
+++ Dictura.WebUI/Program.cs
@@ -0,0 +1,35 @@
+using Dictura.WebUI.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+app.UsePathBase("/dictura");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Dictura.WebUI.Client._Imports).Assembly);
+
+app.Run();
