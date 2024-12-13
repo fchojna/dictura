99180c62d794fb9f34bea67559b9910a4b702fbe
fchojna <filipchojna99@gmail.com>
2024-12-13 09:21:01 +0100

    Add test controller and container configuration
    #75 #83

---
+++ api/Dictura.Api/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dictura.Api.Controllers;
+
+[Route("[controller]")]
+public class TestController : Controller
+{
+    [HttpGet("echo")]
+    public IActionResult Echo()
+    {
+        return Ok("echo");
+    }
+}
